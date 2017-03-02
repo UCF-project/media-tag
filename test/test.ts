4cff2c22a7c084904fd1a0c6ed90df21b391a609
Karine Pires <karine.pires@alterway.fr>
2017-03-02 17:51:01 +0100

    Add jest for tests.

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,14 @@
-import test from 'ava';
+// import test from 'ava';
 
-async function fn() {
-    return Promise.resolve('foo');
-}
+// async function fn() {
+//     return Promise.resolve('foo');
+// }
 
-test(async (t) => {
-    t.is(await fn(), 'foo');
-});
+// test(async (t) => {
+//     t.is(await fn(), 'foo');
+// });
+
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+})
